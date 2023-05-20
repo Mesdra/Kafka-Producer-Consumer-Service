d2a49afca52fead44190942c45d765de1acd91ae
Mesdra <vinicavalari97@gmail.com>
2023-05-20 16:30:24 -0300

    Atualização Consumer

---
+++ str-consumer/src/main/java/br/com/mesdra/strconsumer/custom/StrConsumerCurtomListener.java
@@ -0,0 +1,26 @@
+package br.com.mesdra.strconsumer.custom;
+
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@KafkaListener
+public @interface StrConsumerCurtomListener {
+
+    @AliasFor(annotation = KafkaListener.class,attribute = "topics")
+    String[] topics() default "str_topic";
+
+    @AliasFor(annotation = KafkaListener.class,attribute = "containerFactory")
+    String containerFactory() default "strContainerFactory";
+
+    @AliasFor(annotation = KafkaListener.class,attribute = "groupId")
+    String groupId() default "";
+
+}
--- str-consumer/src/main/java/br/com/mesdra/strconsumer/listners/StrConsumerListener.java
+++ str-consumer/src/main/java/br/com/mesdra/strconsumer/listners/StrConsumerListener.java
@@ -1,6 +1,7 @@
 package br.com.mesdra.strconsumer.listners;
 
 
+import br.com.mesdra.strconsumer.custom.StrConsumerCurtomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -8,9 +9,19 @@ import org.springframework.stereotype.Component;
 @Component
 @Log4j2
 public class StrConsumerListener {
-    @KafkaListener(groupId = "group-1",topics = "str_topic",containerFactory = "strContainerFactory")
-    public void listener(String messsage){
-        log.info("Mensagem Recebida : {}",messsage);
+    @StrConsumerCurtomListener(groupId = "group-1")
+    public void create(String messsage) {
+        log.info("CREATE :: Mensagem Recebida : {}", messsage);
+    }
+
+    @StrConsumerCurtomListener(groupId = "group-1")
+    public void log(String messsage) {
+        log.info("LOG :: Mensagem Recebida : {}", messsage);
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "str_topic", containerFactory = "strContainerFactory")
+    public void history(String messsage) {
+        log.info("HISTORY :: Mensagem Recebida : {}", messsage);
     }
 
 
