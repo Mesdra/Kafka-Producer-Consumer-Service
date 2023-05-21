7e53c0da136a4cb8dcc05868327aaead7759853f
Mesdra <vinicavalari97@gmail.com>
2023-05-21 16:03:18 -0300

    Envio de pagamento finalizado

--- payment-service/src/main/java/br/com/mesdra/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/br/com/mesdra/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,14 +2,27 @@ package br.com.mesdra.paymentservice.service.impl;
 
 import br.com.mesdra.paymentservice.model.Payment;
 import br.com.mesdra.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
     @Override
+    @SneakyThrows
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT SERVICE IMPL ::: Pagamento Recebido {}", payment);
+        log.info("Pagamento Recebido {}", payment);
+        Thread.sleep(1000);
+        log.info("Enviado Pagamento ...");
+        kafkaTemplate.send("payment-topic",payment);
     }
 }
